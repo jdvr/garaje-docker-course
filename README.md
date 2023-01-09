df84e3698958eca2f7b6642a1fc0cab9ee56ce36
jdvr <juandavidvegarguez@gmail.com>
2023-01-09 12:06:28 +0100

    init app without docker

---
+++ README.md
@@ -0,0 +1,7 @@
+Garaje Docker Course
+--
+
+
+This codebase is a dead-simple nodejs app to showcase how to dockerize an application.
+Do not use `index.js` as example of real production application.
+
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { Client } = require('pg');
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+
+const app = express();
+
+app.use(express.json());
+
+const databaseConfig = readDBConfigFromEnv()
+
+const connectionString = `postgres://${databaseConfig.userName}:${databaseConfig.dbPass}@${databaseConfig.dbHost}:${databaseConfig.dbPort}/${databaseConfig.dbName}`;
+
+const insertQuery = 'INSERT INTO IDEA (TITLE, DESCRIPTION) VALUES ($1, $2) RETURNING *';
+const findAllQuery =  'SELECT * FROM IDEA';
+
+const client = new Client({ connectionString });
+client.connect();
+
+
+app.post('/idea', (req, res) => {
+    const { title, description } = req.body;
+
+    const values = [title, description];
+
+    client.query(insertQuery, values, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error writing idea to database' });
+            return;
+        }
+
+        res.json(result.rows[0]);
+    });
+});
+
+app.get('/ideas', (req, res) => {
+
+    client.query(findAllQuery, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error reading ideas from database' });
+            return;
+        }
+
+        res.json(result.rows);
+    });
+})
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
+
+function readDBConfigFromEnv() {
+    const userName = process.env.DB_USERNAME
+    const dbName = process.env.DB_NAME
+    const dbPass = process.env.DB_PASS
+    const dbHost = process.env.DB_HOST || 'localhost'
+    const dbPort = process.env.DB_HOST || 5432
+
+    return {
+        userName, dbName, dbPass, dbHost, dbPort
+    }
+}
